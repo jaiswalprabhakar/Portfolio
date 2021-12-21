fbd5d3e518c9ec62957de4a942e036798e97d20c
jaiswalprabhakar <prabhakarjaiswal8083430254@gmail.com>
2021-12-21 17:50:34 +0530

    dec 21 2021 5:50

--- responsive-portfolio-website-Alexa/assets/js/main.js
+++ responsive-portfolio-website-Alexa/assets/js/main.js
@@ -10,13 +10,24 @@ if(navToggle){
         navMenu.classList.add('show-menu')
     })
 }
-
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
